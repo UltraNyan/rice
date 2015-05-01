51421ea182cb7ed2578cd5c1aa31db9f704b3773
UltraNyan <ultranyan@live.com>
2015-05-01 21:34:06 +0300

    I suck cocks

--- Scripts/toilet-font-list.sh
+++ Scripts/toilet-font-list.sh
@@ -1,1 +1,1 @@
-find /usr/share/figlet -name *.?lf -exec basename {}  \; | sed -e "s/\..lf$//" | xargs -I{} toilet -f {} {}
+find /usr/share/figlet -name *.?lf -exec basename {}  \; | sed -e "s/\..lf$//" | xargs -I{} toilet -tf {} {}
